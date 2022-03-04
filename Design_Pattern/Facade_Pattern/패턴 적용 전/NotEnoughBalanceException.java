98159cbcebf0baf6cbd920948b48e2200d6195a0
dohyoungK <62333468+dohyoungK@users.noreply.github.com>
2022-03-04 17:27:43 +0900

    Add files via upload

---
+++ Design_Pattern/Facade_Pattern/패턴 적용 전/NotEnoughBalanceException.java
@@ -0,0 +1,12 @@
+package before;
+
+public class NotEnoughBalanceException extends RuntimeException {
+	private final Order order;
+	public NotEnoughBalanceException(Order order) {
+		this.order = order;
+	}
+	public Order getOrder() {
+		return order;
+	}
+
+}
